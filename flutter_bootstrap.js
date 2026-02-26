1042e0d26c1f55b1a34630acc73afaa258b5c735
VitaliBobr <VitaliBobr@users.noreply.github.com>
2026-02-26 07:43:53 +0000

    deploy: 087e816dc9fb38519c9f6b697aca87f4368c3c81

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4209685762"
+    serviceWorkerVersion: "2265999998"
   }
 });
